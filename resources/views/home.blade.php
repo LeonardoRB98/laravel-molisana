b68aefea49c196ad55f4b1be10365b09c6b7c28d
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-02-03 16:43:58 +0100

    primo commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="{{ asset('img/marchio-sito-test.png') }}" alt="">
+        <ul class="list-inline">
+            <li><a href="#">home</a></li>
+            <li><a href="#">prodotti</a></li>
+            <li><a href="#">news</a></li>
+        </ul>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+</body>
+</html>
